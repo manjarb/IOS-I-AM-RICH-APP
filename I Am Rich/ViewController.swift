51caadfafb9caeb69ef38f29ed35644ef9e21d91
Varis Darasirikul <varis.dara@gmail.com>
2018-08-16 01:08:52 +0800

    Init project

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Varis Darasirikul on 16/8/2561 BE.
+//  Copyright © 2561 Varis Darasirikul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
